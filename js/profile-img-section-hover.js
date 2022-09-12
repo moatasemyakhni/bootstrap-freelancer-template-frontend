3a8700fd80744f8c12afd32de51a0a9970893584
= <motasem.yakhni@gmail.com>
2022-09-12 16:31:04 +0300

    profile hover  action

---
+++ js/profile-img-section-hover.js
@@ -0,0 +1,9 @@
+const images = document.querySelectorAll('.profile-img-wrapper')
+
+// images.forEach((img) => {
+//     img.addEventListener('mouseover', () => {
+//         let div = document.createElement('div')
+//         div.setAttribute('class', 'profile-img-hover')
+//         img.appendChild(div)
+//     })
+// })
