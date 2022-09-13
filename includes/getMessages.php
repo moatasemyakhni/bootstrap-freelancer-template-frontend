86d854298139fec9dbfaa5ca5eb9526873db460c
= <motasem.yakhni@gmail.com>
2022-09-13 23:21:01 +0300

    create message getter api

---
+++ includes/getMessages.php
@@ -0,0 +1,12 @@
+<?php
+include('dbh.php');
+
+$query = $mysqli->prepare("SELECT message FROM messages");
+$query->execute();
+$messages = $query->get_result();
+$response = [];
+while($message = $messages->fetch_assoc()) {
+    $response[] = $message;
+}
+
+echo json_encode($response);
